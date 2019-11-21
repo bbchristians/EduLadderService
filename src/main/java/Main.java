ec00afb7bf2d87a3ca49f24860907f6176338fad
Ben Christians <bbc7909@rit.edu>
2019-11-21 13:27:54 -0500

    asd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
     	try {
 			controller = new EduLadder();
-		
+
 	        get("/getRankableQuestions", Main::getRankableQuestions);
 	        post("/getQuestion", Main::getQuestion);
 	        post("/submitRankings", Main::submitRankings);
